5e6732679c945f6633195ef602b99ada4aaaa0e7
mohit-walia <59135206+mohit-walia@users.noreply.github.com>
2026-02-11 15:43:06 +0530

    Add IDCardGenerator class

---
+++ helpers/IDCardGenerator.php
@@ -0,0 +1,10 @@
+<?php
+
+class IDCardGenerator {
+    public function generate($name, $id) {
+        // Code to generate ID card
+        return "ID Card for: " . $name . " with ID: " . $id;
+    }
+}
+
+?>
